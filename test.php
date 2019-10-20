944bb3f7d70b2aac90ad3455dc9d95211535a3da
lutinmaviou <bertrandbou@hotmail.fr>
2019-10-20 18:52:05 +0200

    updated test file to test the toto branch

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-echo "n'importe quoi\n";
+echo "salut!";
 echo "ubuntu\n";
 ?>
 <script src="https://cdn.tiny.cloud/1/z5cswllgf3io3usrwa5xosso9z5fo9ek0vfp6r7my9cmyx0u/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
