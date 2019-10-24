753283625f64221eb3806ce2da14f5921da989f6
lutinmaviou <bertrandbou@hotmail.fr>
2019-10-24 16:17:38 +0200

    created autoloader file

---
+++ controllers/Autoloader.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Lmv\writerBlog\Models;
+
+class Autoloader
+{
+    static function register()
+    {
+        // spl_autoload_register prend en paramètre la fonction permettant d'inclure 
+        // les classes nécessaires
+        spl_autoload_register(array(__CLASS__, 'autoload'));
+    }
+    static function autoload($class)
+    {
+        $class = str_replace(__NAMESPACE__ . '\\', '', $class);
+        $class = str_replace('\\', '/', $class);
+        require 'models/' . $class . '.php';
+    }
+}
