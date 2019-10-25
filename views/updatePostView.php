60319c58b4e88b77076f68d1b4b93801282d4b10
lutinmaviou <bertrandbou@hotmail.fr>
2019-10-25 15:47:17 +0200

    updated updatePostView design fixed

--- views/updatePostView.php
+++ views/updatePostView.php
@@ -1,19 +1,27 @@
 <?php $title = 'Modification de post';
 ob_start();
 ?>
-<form action='index.php?action=submitUpdatePost&amp;id=<?= $post['id'] ?>' method='POST'>
-    <label for='title'>Titre :
-        <input type="text" name='title' class="editor" id='title' value="<?= $post['title'] ?>">
-    </label>
-    <label for="chapterContent">Contenu :
-        <textarea name="chapterContent" class="editor" id="chapterContent" cols="30" rows="10"><?= $post['chapterContent'] ?></textarea>
-    </label>
-    <button>Valider</button>
-</form>
+<section class="container">
+    <h2 class="text-center mt-5">Modifier</h2>
+    <div class="row">
+        <form class="col-lg-10 text-center mx-auto bg-light border border-light rounded-lg p-5 mt-4 shadow" action="index.php?action=submitUpdatePost&amp;id=<?= $post['id'] ?>" method="POST">
+            <div class="form-row mb-4">
+                <div class="mx-auto">
+                    <label for="title">
+                        <input type="text" id="title" name="title" value="<?= $post['title'] ?>" class="form-control mr-lg-5" required>
+                    </label>
+                </div>
+            </div>
+            <label for="chapterContent" class="col-lg-12">
+                <textarea name="chapterContent" class="tinyMce" id="chapterContent" rows="10" required><?= $post['chapterContent'] ?></textarea>
+            </label>
+            <small class="form-text text-muted mb-4">
+            </small>
+            <button class="btn btn-outline-info px-5" type="submit">Envoyer</button>
+        </form>
+    </div>
+</section>
 <?php
-var_dump($post['id']);
-var_dump($_GET['id']);
-
 $content = ob_get_clean();
 require('template.php');
 ?>
