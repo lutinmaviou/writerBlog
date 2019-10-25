53af3376edb600b2b04e9c1ad5803e1f943bc4ad
lutinmaviou <bertrandbou@hotmail.fr>
2019-10-25 17:26:38 +0200

    deleted unused files

--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "salut!";
-echo "ubuntu\n";
-?>
-<script src="https://cdn.tiny.cloud/1/z5cswllgf3io3usrwa5xosso9z5fo9ek0vfp6r7my9cmyx0u/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
