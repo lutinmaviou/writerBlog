2f2487209f5db1e9e91c57aac2b76c9729b2e83e
lutinmaviou <bertrandbou@hotmail.fr>
2019-10-31 22:23:01 +0100

    updated DbConnect config putenv() getenv() fixed

--- models/DbConnect.php
+++ models/DbConnect.php
@@ -2,14 +2,13 @@
 
 namespace Lmv\WriterBlog\Models;
 
-require 'security/config.php';
-
 class DbConnect
 {
     protected function _dbConnect()
     {
         try {
-            $db = new \PDO('mysql:host=' . getenv('SQLHOST') . ';dbname=blog;charset=utf8', getenv('SQL_USER'), getenv('SQL_PSWD'));
+            require 'security/config.php';
+            $db = new \PDO('mysql:host=' . $SQL_host . ';dbname=' . getenv('SQL_DBNAME') . ';charset=utf8', getenv('SQL_USER'), getenv('SQL_PSWD'));
             $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
             return $db;
         } catch (Exception $e) {
@@ -17,10 +16,8 @@ class DbConnect
         }
     }
 }
-//echo getenv('SQL_USER');
-//putenv('DSN=mysql:host=db5000206120.hosting-data.io;dbname=dbs201063;charset=utf8');
-//phpinfo();
-
 // $db = new \PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
 // $db = new \PDO('mysql:host=db5000206120.hosting-data.io;dbname=dbs201063;charset=utf8', 'dbu330117', 'writerBlog_123');
+// $db = new \PDO('mysql:host=' . getenv('SQL_HOST') . ';dbname=' . getenv('SQL_DBNAME') . ';charset=utf8', getenv('SQL_USER'), getenv('SQL_PSWD'));
 // AuthUserFile "/homepages/12/d771396630/htdocs/writerBlog/chemin.php\security\ht.passwd"
+// $db = new \PDO('mysql:host=' . $SQL_host . ';dbname=' . getenv('SQL_DBNAME') . ';charset=utf8', getenv('SQL_USER'), getenv('SQL_PSWD'));
--- security/config.php
+++ security/config.php
@@ -1,4 +1,14 @@
 <?php
-$SQLhost = putenv('SQL_HOST=localhost');
-$SQLpass = putenv('SQL_USER=root');
-$SQLlogin = putenv('SQL_PSWD=' . '');
+putenv('SQL_HOST=db5000206120.hosting-data.io');
+putenv('SQL_DBNAME=dbs201063');
+putenv('SQL_USER=dbu330117');
+putenv('SQL_PSWD=writerBlog_123');
+$SQL_host = getenv('SQL_HOST');
+$SQL_dbName = getenv('SQL_DBNAME');
+$SQL_user = getenv('SQL_USER');
+$SQL_pswd = getenv('SQL_PSWD');
+
+/*$SQL_host = 'localhost';
+$SQL_dbName = 'blog';
+$SQL_user = 'root';
+$SQL_pswd = '';*/
