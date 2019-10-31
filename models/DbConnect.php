2c1df8e2d22acc2a366888666954b549aa4423ca
lutinmaviou <bertrandbou@hotmail.fr>
2019-10-31 22:58:03 +0100

    created .gitignore

--- models/DbConnect.php
+++ models/DbConnect.php
@@ -8,7 +8,7 @@ class DbConnect
     {
         try {
             require 'security/config.php';
-            $db = new \PDO('mysql:host=' . $SQL_host . ';dbname=' . getenv('SQL_DBNAME') . ';charset=utf8', getenv('SQL_USER'), getenv('SQL_PSWD'));
+            $db = new \PDO('mysql:host=' . $SQL_host . ';dbname=' . $SQL_dbName . ';charset=utf8', $SQL_user, $SQL_pswd);
             $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
             return $db;
         } catch (Exception $e) {
@@ -18,6 +18,6 @@ class DbConnect
 }
 // $db = new \PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
 // $db = new \PDO('mysql:host=db5000206120.hosting-data.io;dbname=dbs201063;charset=utf8', 'dbu330117', 'writerBlog_123');
-// $db = new \PDO('mysql:host=' . getenv('SQL_HOST') . ';dbname=' . getenv('SQL_DBNAME') . ';charset=utf8', getenv('SQL_USER'), getenv('SQL_PSWD'));
 // AuthUserFile "/homepages/12/d771396630/htdocs/writerBlog/chemin.php\security\ht.passwd"
+// AuthUserFile "C:\wamp64\www\writerBlog\security\ht.passwd"
 // $db = new \PDO('mysql:host=' . $SQL_host . ';dbname=' . getenv('SQL_DBNAME') . ';charset=utf8', getenv('SQL_USER'), getenv('SQL_PSWD'));
