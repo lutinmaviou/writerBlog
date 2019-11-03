93e5e968a610fb264bcf7c97c74219a1c85cce90
lutinmaviou <bertrandbou@hotmail.fr>
2019-11-03 13:18:53 +0100

    moved autoloader emplacement vendor folder

--- vendor/Autoloader.php
+++ vendor/Autoloader.php
@@ -4,21 +4,14 @@ namespace Lmv\WriterBlog;
 
 class Autoloader
 {
-    //private $models;
     static function register()
     {
         spl_autoload_register(array(__CLASS__, 'autoload'));
     }
     static function autoload($class)
     {
-        //var_dump($class); // Lmv\WriterBlog\Models\PostsMngr
         $class = str_replace(__NAMESPACE__ . '\Models\\', '', $class);
         //$class = str_replace('\\', '/', $class);
-        //var_dump($class); // PostsMngr
-        //var_dump(__NAMESPACE__); // Lmv\WriterBlog
-        //var_dump(__FILE__); // C:\wamp64\www\writerBlog\Autoloader.php
-        //var_dump(__DIR__); // C:\wamp64\www\writerBlog
-
         require 'models/' . $class . '.php';
     }
 }
